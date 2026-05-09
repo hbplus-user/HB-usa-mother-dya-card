c225aeb5d0989f7548b82c0e8fb3a42e8da42f09
Subit125 <subitpradhan125@gmail.com>
2026-05-09 13:01:39 +0530

    Update CTA link and prepare for push to new repo

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -33,7 +33,7 @@ export const CTA = () => {
           </p>
 
           <motion.a
-            href="https://pages.razorpay.com/pl_SmsT4cx1TF0I0k/view"
+            href="https://rzp.io/rzp/X9orrWD"
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ scale: 1.03 }}
